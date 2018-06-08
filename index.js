bc628a69eacc44b2a1ef5b8c4a1e2b8f9e4655ed
RBG74 <remy.bernardgranger@gmail.com>
2018-06-08 10:25:46 +0200

    Reworked some functions and added some tests

--- index.js
+++ index.js
@@ -6,27 +6,47 @@ const fs = require("fs");
 const inputFolder = "./files/";
 const outputFolder = "./zips/";
 
+//Zips all files in inputDir and subdirs to the outputDir
 const zipFiles = async function(inputDir, outputDir) {
-    try {
-        const files = await getFilesRecursively(inputDir);
-        files.forEach(file => {
-            const filepath = file.split("/");
-            const filename = filepath[filepath.length - 1];
-            const input = file;
-            const output = outputDir + filename;
-            zipFile(input, output);
-        });
-    } catch (error) {
-        console.error("Error caught while ziping files:", error);
-    }
+    return new Promise(async(resolve, reject) => {
+        try {
+            const files = await getFilesRecursively(inputDir);
+            const promises = [];
+            files.forEach(file => {
+                const filepath = file.split("/");
+                const filename = filepath[filepath.length - 1];
+                const input = file;
+                const output = outputDir + filename;
+                promises.push(zipFile(input, output));
+            });
+            Promise.all(promises).then(()=>{
+                resolve();
+            });
+        } catch (error) {
+            console.error("Error caught while ziping files:", error);
+            reject (error);
+        }
+    });
+    
 };
 exports.zipFiles = zipFiles;
 
+//Zips a single file
 const zipFile = function(input, output) {
-    const inp = fs.createReadStream(input);
-    const out = fs.createWriteStream(output + ".gz");
-    console.log(input + " compressed as " + output + ".gz");
-    inp.pipe(gzip).pipe(out);
+    return new Promise((resolve, reject) => {
+        const inp = fs.createReadStream(input);
+        const out = fs.createWriteStream(output + ".gz");
+        inp.pipe(gzip)
+            .pipe(out)
+            .on("error", function(err) {
+                console.log("wtf error", err);
+                reject(err);
+            })
+            .on("finish", () => {
+                //console.log(input + " compressed as " + output + ".gz");
+                resolve();
+            });
+    });
 };
 exports.zipFile = zipFile;
 
--- index.test.js
+++ index.test.js
@@ -1,9 +1,35 @@
+const fs = require("fs");
 const index = require("./index.js");
 
-test("getFilesRecursively should find 7 files", () => {
+it("getFilesRecursively should find 7 files", () => {
     index.getFilesRecursively("./files/").then(files => {
-        console.log(files);
         expect(files.length).toBe(7);
     });
 });
 
+it("zipFile should create ./zips/test1/files1.txt.gz", () => {
+    index.zipFile("./files/file1.txt", "./zips/test1/file1.txt").then(() => {
+        index.getFilesRecursively("./zips/test1/").then(files => {
+            expect(files[0]).toBe("./zips/test1/file1.txt.gz");
+            removeFile("./zips/test1/file1.txt.gz")
+        });
+    });
+});
+
+it("zipFiles should create 7 files", () => {
+    index.zipFiles("./files/", "./zips/test2/").then(() => {
+        index.getFilesRecursively("./zips/test2/").then(files => {
+            expect(files.length).toBe(7);
+            files.forEach(file => {
+                removeFile(file);
+            });
+            
+        });
+    });
+});
+
+function removeFile(filepath) {
+    fs.unlink(filepath, err => {
+        if (err) throw err;
+    });
+}
